65803b27443acdf169ca71f65513e2862daef831
danielbrainich <143758962+danielbrainich@users.noreply.github.com>
2024-02-21 20:02:14 -0800

    AlertModal works on add new trail. Still need to protect enpoints on backend as needed.

--- my-react-app/src/components/Trails.js
+++ my-react-app/src/components/Trails.js
@@ -2,10 +2,13 @@ import React, { useRef, useEffect, useState } from 'react';
 import useCsrfToken from '../hooks/useCsrfToken';
 import useAuth from '../hooks/useAuth';
 import NewTrails from './TrailsNew';
+import AlertModal from './AlertModal';
+import { useAuthContext } from '../contexts/AuthContext';
 
 
 function ListTrails() {
   const [trailSuccess, setTrailSuccess] = useState(false);
+  const { user } = useAuthContext();
 
 return (
   <div className="container mt-3 mt-md-5">
@@ -25,7 +28,12 @@ return (
                     <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
                   <div className="modal-body">
-                  <NewTrails setTrailSuccess={setTrailSuccess} trailSuccess={trailSuccess} />
+                    {user ? (
+                      <NewTrails setTrailSuccess={setTrailSuccess} trailSuccess={trailSuccess} />
+                    ) : (
+                      <AlertModal title="Hello!" feature="create a new trail" />
+                    )
+                    }
                   </div>
                 </div>
               </div>
@@ -33,20 +41,20 @@ return (
           </div>
 
           <div>
-            <ul class="nav nav-tabs" id="myTab" role="tablist">
-              <li class="nav-item" role="presentation">
-                <button class="nav-link active" id="my-trails-tab" data-bs-toggle="tab" data-bs-target="#my-trails-tab-pane" type="button" role="tab" aria-controls="my-trails-tab-pane" aria-selected="true">My Trails</button>
+            <ul className="nav nav-tabs" id="myTab" role="tablist">
+              <li className="nav-item" role="presentation">
+                <button className="nav-link active" id="my-trails-tab" data-bs-toggle="tab" data-bs-target="#my-trails-tab-pane" type="button" role="tab" aria-controls="my-trails-tab-pane" aria-selected="true">My Trails</button>
               </li>
-              <li class="nav-item" role="presentation">
-                <button class="nav-link" id="saved-trails-tab" data-bs-toggle="tab" data-bs-target="#saved-trails-tab-pane" type="button" role="tab" aria-controls="saved-trails-tab-pane" aria-selected="false">Saved Trails</button>
+              <li className="nav-item" role="presentation">
+                <button className="nav-link" id="saved-trails-tab" data-bs-toggle="tab" data-bs-target="#saved-trails-tab-pane" type="button" role="tab" aria-controls="saved-trails-tab-pane" aria-selected="false">Saved Trails</button>
               </li>
             </ul>
-            <div class="tab-content" id="myTabContent">
-              <div class="tab-pane fade show active" id="my-trails-tab-pane" role="tabpanel" aria-labelledby="my-trails-tab" tabindex="0">
-                <div>This is where all of my trails will live</div>
+            <div className="tab-content" id="myTabContent">
+              <div className="tab-pane fade show active" id="my-trails-tab-pane" role="tabpanel" aria-labelledby="my-trails-tab" tabindex="0">
+                <div className="mt-3">This is where all of my trails will live</div>
               </div>
-              <div class="tab-pane fade" id="saved-trails-tab-pane" role="tabpanel" aria-labelledby="saved-trails-tab" tabindex="0">
-                <div>This is where everyone else's information will live</div>
+              <div className="tab-pane fade" id="saved-trails-tab-pane" role="tabpanel" aria-labelledby="saved-trails-tab" tabindex="0">
+              <div className="mt-3">This is where everyone else's trails will live</div>
               </div>
             </div>
           </div>
