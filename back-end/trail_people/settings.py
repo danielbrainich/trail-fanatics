4a04288bc569d76bb65ae4acfddb2ecc0fbf9b6e
danielbrainich <143758962+danielbrainich@users.noreply.github.com>
2024-03-02 14:46:17 -0800

    changed middleware order for debugging prod

--- back-end/trail_people/settings.py
+++ back-end/trail_people/settings.py
@@ -46,14 +46,15 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
+    'django.middleware.csrf.CsrfViewMiddleware', # <---- Added this 
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "trail_people.urls"
