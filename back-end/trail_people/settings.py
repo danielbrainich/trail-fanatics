abc44d38bafb6316eceb97e53d360a500edbc1ec
danielbrainich <143758962+danielbrainich@users.noreply.github.com>
2024-03-01 16:58:39 -0800

    debugging deploy

--- back-end/trail_people/settings.py
+++ back-end/trail_people/settings.py
@@ -74,16 +74,23 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "trail_people.wsgi.application"
 
+import os
+import dj_database_url
+from pathlib import Path
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Common settings for all environments
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# Heroku (Production) settings
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
