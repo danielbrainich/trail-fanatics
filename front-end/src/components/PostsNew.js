464dde4ed3056c87c1a32bf45246af9f9372b257
danielbrainich <143758962+danielbrainich@users.noreply.github.com>
2024-03-03 09:26:01 -0800

    removed mention of csrf from post new function

--- front-end/src/components/PostsNew.js
+++ front-end/src/components/PostsNew.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import useCsrfToken from '../hooks/useCsrfToken';
+// import useCsrfToken from '../hooks/useCsrfToken';
 import useAuth from '../hooks/useAuth';
 import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
 import config from '../config';
@@ -17,7 +17,7 @@ function NewPosts({ setPostSuccess, postSuccess, setTagsList, tagsList}) {
     trailDescription: '',
     trailCoordinates: null,
   });
-  const {csrfToken, updateCsrfToken} = useCsrfToken();
+  // const {csrfToken, updateCsrfToken} = useCsrfToken();
   const [markerPosition, setMarkerPosition] = useState(null);
   const [showMap, setShowMap] = useState(false);
   const { isLoaded, loadError } = useJsApiLoader({
@@ -68,15 +68,15 @@ function NewPosts({ setPostSuccess, postSuccess, setTagsList, tagsList}) {
 
     const apiUrl = `${config.API_BASE_URL}/content/posts/`;
 
-    if (!csrfToken) {
-      await updateCsrfToken();
-    }
+    // if (!csrfToken) {
+    //   await updateCsrfToken();
+    // }
     const fetchConfig = {
       method: "POST",
       body: JSON.stringify(payload),
       headers: {
         "Content-Type": "application/json",
-        "X-CSRFToken": csrfToken.csrfToken,
+        // "X-CSRFToken": csrfToken.csrfToken,
       },
       credentials: 'include',
     };
