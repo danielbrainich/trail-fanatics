dc97ac2a0c778c8a8704af335cb5b1e8015f2b17
danielbrainich <143758962+danielbrainich@users.noreply.github.com>
2024-03-03 08:09:20 -0800

    wildcard not accepted. added heroku and netlify origins

--- back-end/trail_people/settings.py
+++ back-end/trail_people/settings.py
@@ -42,9 +42,12 @@ if not DEBUG:
     CSRF_COOKIE_SECURE = True
     SECURE_SSL_REDIRECT = True
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-    CSRF_TRUSTED_ORIGINS = ['*']
 
-
+CSRF_TRUSTED_ORIGINS = [
+    'https://main--trail-people.netlify.app',
+    'http://localhost:3000',
+    'http://trail-people-793a505ff939.herokuapp.com'
+]
 
 # Application definition
 
