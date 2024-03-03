2a441c8fbe268578cd942fa7fc69c810fd662a4c
danielbrainich <143758962+danielbrainich@users.noreply.github.com>
2024-03-02 17:14:57 -0800

    debug

--- back-end/trail_people/settings.py
+++ back-end/trail_people/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -54,6 +53,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "trail_people.urls"
@@ -148,21 +148,17 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://main--trail-people.netlify.app",
     "http://main--trail-people.netlify.app",
-    "https://trail-people-793a505ff939.herokuapp.com",
-    "http://trail-people-793a505ff939.herokuapp.com",
-
 ]
 
-CSRF_TRUSTED_ORIGINS = ["*"]
-
-    # "http://localhost:3000",
-    # "https://main--trail-people.netlify.app",
-    # "http://main--trail-people.netlify.app",
-    # "https://trail-people-793a505ff939.herokuapp.com",
-    # "http://trail-people-793a505ff939.herokuapp.com",
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+    "https://main--trail-people.netlify.app",
+    "http://main--trail-people.netlify.app",
+]
 
 if DEBUG:
     LOGIN_REDIRECT_URL = "http://localhost:3000"
+
 else:
     LOGIN_REDIRECT_URL = "https://main--trail-people.netlify.app"
 
